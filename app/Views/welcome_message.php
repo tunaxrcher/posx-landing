de87630b0a6f121e7baf529e11ad8e7f3579058e
tunaxrcher <mystic.outriderxpaine@gmail.com>
2025-04-03 13:21:59 +0700

    init

---
+++ app/Views/welcome_message.php
@@ -0,0 +1,131 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- META TAGS -->
+    <meta charset="utf-8">
+    <meta name="author" content="AtypicalThemes">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"> 
+
+    <!-- WEBSITE TITLE & DESCRIPTION -->
+    <title>POSX - POSX ผู้ช่วยร้านอาหาร Online</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <!-- OG meta tags that improve the look of your post on social media -->
+    <meta property="og:site_name" content="" /><!--website name-->
+    <meta property="og:site" content="" /> <!--website link-->
+    <meta property="og:title" content=""/> <!--Post title-->
+    <meta property="og:description" content="" /> <!--Post description-->
+    <meta property="og:image" content="" /><!-- Image link (jpg only)-->
+    <meta property="og:url" content="" /> <!--where do you want your post to link to-->
+    <meta property="og:type" content="article" />
+
+    <!-- Favicon and Apple Icons -->
+    <link rel="shortcut icon" href="images/favicon/favicon.ico" type="image/x-icon" />
+    <link rel="apple-touch-icon" href="images/favicon/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon" sizes="76x76" href="images/favicon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="images/favicon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon" sizes="120x120" href="images/favicon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="images/favicon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon" sizes="152x152" href="images/favicon/apple-touch-icon-152x152.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon-180x180.png" />
+
+    <!-- STYLES -->
+    <!-- Bootstrap -->
+    <link href="https://www.atypicalthemes.com/portfolio/strider_demo/pages/StriderDark/Particles/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Main Stylesheet -->
+    <link href="https://www.atypicalthemes.com/portfolio/strider_demo/pages/StriderDark/Particles/css/style.css" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
+
+<style>
+        /** BASE **/
+        * {
+            font-family: 'Kanit', sans-serif;
+        }
+
+        a {
+            font-family: 'Kanit', sans-serif;
+        }
+
+        html, body {
+            height: 100%;
+            margin: 0;
+            overflow-y: hidden;
+        }
+
+        .test {
+            height: 100%;
+        }
+
+    </style>
+</head>
+<body>
+
+<div class="test">
+    <!-- Loading Screen -->
+    <div id="loader-wrapper">
+        <h1 class="loader-logo"><img style="height: 25px;" class="img-fluid" src="images/logo.png" alt="site logo"></h1>
+        <div id="progress"></div>
+        <h3 class="loader-text">LOADING</h3>
+    </div>
+    
+    <!-- //// HEADER //// -->
+    <header id="main-header">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-dark">
+            <div class="container">
+                <!-- Site Logo -->
+                <!-- <a id="logo" class="navbar-brand" href="#"><img style="height: 25px;" class="img-fluid" src="images/logo.png" alt="site logo"></a> -->
+                <!-- Dropdown Button -->
+                <button id="hamburger" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                </button>
+                <!-- Navigation Links -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <script>
+                                function handle() {
+                                    alert("Coming Soon");
+                                }
+                            </script>
+                            <a class="nav-link" href="#" onclick="handle()">ติดต่อ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://app.posx.co">เข้าสู่ระบบ</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header><!-- Header End -->
+    
+    <!-- /// HERO SECTION /// -->
+    <div id="hero-section" class="small-margin">
+        <div id="particles-web"></div>
+        <div class="row hero-unit">
+            <div class="container">
+                <div class="hero-caption"><!-- Main Tagline -->
+                    <h1><img style="height: 25px;" class="img-fluid" src="images/logo.png" alt="site logo"></h1>
+                    <hr>
+                    ผู้ช่วยร้านอาหาร Online
+                </div>
+            </div>
+        </div>
+    </div><!-- Hero Section End -->
+</div>
+    
+    <!-- //// SCRIPTS //// -->
+    <script src="https://www.atypicalthemes.com/portfolio/strider_demo/pages/StriderDark/Particles/js/jquery-3.2.1.min.js"></script>
+    <script src="https://www.atypicalthemes.com/portfolio/strider_demo/pages/StriderDark/Particles/js/popper.min.js"></script>
+    <script src="https://www.atypicalthemes.com/portfolio/strider_demo/pages/StriderDark/Particles/js/bootstrap.min.js"></script>
+    <script src="https://www.atypicalthemes.com/portfolio/strider_demo/pages/StriderDark/Particles/js/particles.js"></script>
+    <script src="https://www.atypicalthemes.com/portfolio/strider_demo/pages/StriderDark/Particles/js/isotope.pkgd.min.js"></script>
+    <script src="https://www.atypicalthemes.com/portfolio/strider_demo/pages/StriderDark/Particles/js/strider.js"></script>
+</body>
+</html>
